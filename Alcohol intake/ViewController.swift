a7eba3cdb1c12dbbb1ef1c8775582f6d893a7f2a
ドコモ <docomo@dokomo-no-macbook-air.local>
2017-01-09 13:54:29 +0900

    ・仮のUI ・ビールのアルコール量計算完了

--- Alcohol intake/ViewController.swift
+++ Alcohol intake/ViewController.swift
@@ -9,7 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    func Intake(forPercentage percentge:Double, amount:Double) -> Double{
+        let result = percentge * amount
+        return result
+    }
+    
+    @IBAction func drinkBeer(_ sender: Any)  {
+        let beer = Intake(forPercentage:0.05, amount:2)
+        var resultValue:Double = 0
+        resultValue += resultValue + beer
+        print(resultValue)
+        
+    }
+    
 
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
