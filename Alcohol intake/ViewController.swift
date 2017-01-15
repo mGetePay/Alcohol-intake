e874072e2766a915326a1bd902fee85c348e65c0
ドコモ <docomo@dokomo-no-macbook-air.local>
2017-01-15 18:00:52 +0900

    Add 各ドリンクのアルコール量計算
    ビール以外のドリンクの摂取アルコール量を取得し、
    摂取アルコール量の合計を取得できるように機能追加

--- Alcohol intake/ViewController.swift
+++ Alcohol intake/ViewController.swift
@@ -10,21 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    func Intake(forPercentage percentge:Double, amount:Double) -> Double{
-        let result = percentge * amount
-        return result
-    }
-    
-    @IBAction func drinkBeer(_ sender: Any)  {
-        let beer = Intake(forPercentage:0.05, amount:2)
-        var resultValue:Double = 0
-        resultValue += resultValue + beer
-        print(resultValue)
-        
-    }
-    
+var resultValue:Double = 0
 
     
+//各ドリンクのボタン押下で各そのドリンクを一杯飲んだ時のアルコール量を計算する。またその結果を加算し、合計摂取アルコール量を計算する。
+    @IBAction func drinkBeer(_ sender: Any)  {
+        let beer:Double = 15
+                resultValue = resultValue + beer
+    print(resultValue)
+    }
+
+    @IBAction func drinkNihonsyu(_ sender: Any) {
+        let nihonsyu:Double = 27
+        resultValue = resultValue + nihonsyu
+        print(resultValue)
+    }
+
+ 
+    @IBAction func drinkSyochu(_ sender: Any) {
+        let syochu:Double = 20
+        resultValue = resultValue + syochu
+        print(resultValue)
+    }
+    
+    @IBAction func drinkWine(_ sender: Any) {
+        let wine:Double = 18.75
+        resultValue = resultValue + wine
+        print(resultValue)
+    }
+
+
+    @IBAction func drinkHighball(_ sender: Any) {
+        let highball:Double = 9.6
+        resultValue = resultValue + highball
+        print(resultValue)
+    }
+    
+        @IBAction func drinkChuhai(_ sender: Any) {
+        let chuhai:Double = 12.5
+        resultValue = resultValue + chuhai
+        print(resultValue)
+    }
     
     
     
